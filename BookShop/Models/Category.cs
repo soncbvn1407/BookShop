e225f60fb7695c5fdd7b7a88d732c3a14d5abb80
Nguyen Thai Son <sonntgch190883@fpt.edu.vn>
2022-08-16 10:27:00 +0700

    gg

--- BookShop/Models/Category.cs
+++ BookShop/Models/Category.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-//relationship: Category - Book: Many To Many
 namespace BookShop.Models
 {
     public class Category
     {
         public int Id { get; set; } //PK
+
+        [Required]
+        [Display(Name = "Category Name")]
+        [MinLength(3, ErrorMessage = "Category Name must be at least 3 characters")]
+        [MaxLength(30, ErrorMessage = "Category Name cannot be more than 30 characters")]
         public string Name { get; set; }
 
         //set thuộc tính liên kết sang bảng Book
+        //relationship: Category - Book: 1 To Many
         public ICollection<Book> Books { get; set; }
 
     }
