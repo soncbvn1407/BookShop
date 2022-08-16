2a00524c8a5654af099da3d6787954edb913bbf4
Nguyen Thai Son <sonntgch190883@fpt.edu.vn>
2022-08-16 09:16:30 +0700

    Add project files.

---
+++ BookShop/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+//relationship: Category - Book: 
+namespace BookShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } //PK
+        public string Name { get; set; }
+
+        //set thuộc tính liên kết sang bảng Book
+        public ICollection<Book> Books { get; set; }
+
+    }
+}
